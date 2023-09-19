6c79e50a4fc29bfec014c6d05af1671fe6aa17ce
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-19 21:18:39 +0300

    removed frontend

--- public/Phonebook/src/components/Notification.jsx
+++
@@ -1,7 +0,0 @@
-export const Notification = ({ toastMessage }) => {
-  if (toastMessage === null) {
-    return null;
-  }
-  const style = toastMessage.split("")[0] !== "P" ? "success" : "error";
-  return <div className={style}>{toastMessage}</div>;
-};
